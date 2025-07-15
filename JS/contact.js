a6c8f5afdc946722a2edadaf5bb38cb2c2bc763e
BiswajitMakhal <makhalbiswajit1@gmail.com>
2025-07-16 00:53:14 +0530

    hamburger toggle

--- JS/contact.js
+++ JS/contact.js
@@ -1,6 +1,13 @@
+
 $(document).ready(function () {
-    $("#nav-toggler").click(function () {
-        $("#mobile-nav").slideToggle("slow");
+    $('#nav-toggler').click(function () {
+        $('#mobile-nav').addClass('active');
+        $('#overlay').addClass('active');
+    });
+
+    $('#nav-close, #overlay').click(function () {
+        $('#mobile-nav').removeClass('active');
+        $('#overlay').removeClass('active');
     });
 });
 
